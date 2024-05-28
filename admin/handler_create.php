3f4755b0fe14e0b175b7c3345bbc163b648e057e
bluebanana21 <m.affanfahrozi@gmail.com>
2024-05-28 20:38:39 +0700

    postingan masuk db

--- admin/handler_create.php
+++ admin/handler_create.php
@@ -1,15 +1,28 @@
-<?php 
+<?php
 
 if (isset($_POST["create"])) {
-    $title = $_POST["title"];
-    $summary = $_POST["summary"];
-    $content = $_POST["content"];
-    $date = $_POST["date"];
+    include ("../app/koneksi.php");
+
+    $title = mysqli_real_escape_string($koneksi, $_POST["title"]);
+    $summary = mysqli_real_escape_string($koneksi, $_POST["summary"]);
+    $content = mysqli_real_escape_string($koneksi, $_POST["content"]);
+    $date = mysqli_real_escape_string($koneksi, $_POST["date"]);
 
     echo $title;
     echo $summary;
     echo $content;
     echo $date;
+
+    $create_insert = "INSERT INTO posts
+    (title, summary, content, date) 
+    VALUES 
+    ('$title', '$summary', '$content', '$date')";
+
+    if(mysqli_query($koneksi, $create_insert)){
+        echo "masuk db";
+    }else {
+        die("data tidak masuk");
+    }
 }
 
 ?>
